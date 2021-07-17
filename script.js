7ebd7bd3dd1358011b39a34bb8f2549626084a3d
Joyce Calado <joyce.calado@assertivainformacoes.com>
2021-07-17 10:07:47 -0300

    Primeira versão da lp

---
+++ script.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("form")
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert("Obrigada por sua inscrição!💚")
+let nome = document.getElementById("nome").value;
+let email = document.getElementById("email").value;
+let data = {
+    nome,
+    email,
+}
+
+let convertData = JSON.stringify(data);
+
+localStorage.setItem('lead', convertData);
+
+let content = document.getElementById("content");
+
+/*
+let carregando = "<p>"carregando.."</p>" 
+
+content.innerHTML = carregando */
+})
